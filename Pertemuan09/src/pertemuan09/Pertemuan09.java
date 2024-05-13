f749bb1fd877be8c75ca547b3a091c13604980c3
putudian2004 <164039815+putudian2004@users.noreply.github.com>
2024-05-13 12:17:50 +0800

    Add files via upload

---
+++ Pertemuan09/src/pertemuan09/Pertemuan09.java
@@ -0,0 +1,22 @@
+
+package pertemuan09;
+
+/**
+ *
+ * @author Dian Sariani (2201010254)
+ * TGL : 2024-05-13
+ */
+public class Pertemuan09 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//       new frm01().setVisible(true);
+        frm01 f1 = new frm01();
+        f1.setResizable(false);
+        f1.setAlwaysOnTop(true);
+        f1.setVisible(true);
+    }
+    
+}
